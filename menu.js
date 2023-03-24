66ae3b44171792c5ff61ec2ba5bb333a7b0f3478
Kuss Mich <126546688+antofvas@users.noreply.github.com>
2023-03-23 22:20:53 -0300

    diseños de las paginas

--- menu.js
+++ menu.js
@@ -1,3 +1,18 @@
 function redireccion(){
     location.href = "index.html"
     }
+    let menuToggle = document.querySelector('.menu-toggle');
+    let menuToggle = document.querySelector('.menu-toggle i');
+    let menuToggle = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', e=> (
+        menu.classlist.toggle ('show');
+
+        if (menu.classslist.contains('show')){
+            menuToggleIcon.setAtribute('class', 'fa fa-times');
+        }else {
+            menuToggleIcon.setAtribute('class', 'fa fa-bars);
+        }
+
+        )
+    ))
